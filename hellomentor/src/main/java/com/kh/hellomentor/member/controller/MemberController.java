2e38ddbc85c3deda25ee1037ae4f6450d876bd63
재린 <imjaelin@gmail.com>
2023-08-29 16:11:56 +0900

    구현테스트1

--- hellomentor/src/main/java/com/kh/hellomentor/member/controller/MemberController.java
+++ hellomentor/src/main/java/com/kh/hellomentor/member/controller/MemberController.java
@@ -1,4 +1,34 @@
 package com.kh.hellomentor.member.controller;
 
+import com.kh.hellomentor.board.model.vo.Board;
+import com.kh.hellomentor.member.model.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+
+@Controller
 public class MemberController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+
+    @Autowired
+    private MemberService memberService;
+
+    @RequestMapping("/profile_my_post")
+    public String profileMyPost(Model model) {
+        int userNo = 2;
+
+        List<Board> myPosts = memberService.getPostsByUserNo(userNo);
+        model.addAttribute("myPosts", myPosts);
+
+        logger.info("myPosts: {}", myPosts);
+
+        return "mypage/profile_my_post";
+    }
 }
--- hellomentor/src/main/java/com/kh/hellomentor/member/model/service/MemberServiceImpl.java
+++ hellomentor/src/main/java/com/kh/hellomentor/member/model/service/MemberServiceImpl.java
@@ -1,4 +1,22 @@
 package com.kh.hellomentor.member.model.service;
 
-public class MemberServiceImpl implements MemberService{
+import com.kh.hellomentor.board.model.vo.Board;
+import com.kh.hellomentor.member.model.dao.MemberDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+    @Autowired
+    private MemberDao memberDao;
+
+    @Override
+    public List<Board> getPostsByUserNo(int userNo) {
+        return memberDao.getPostsByUserNo(userNo);
+    }
 }
+
+
