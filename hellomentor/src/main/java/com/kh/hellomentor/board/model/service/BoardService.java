97497f1a3f84ef31442d62f13eb88d5242ba3c6b
Chanwoo Lee <saintchung2@naver.com>
2023-08-30 21:35:51 +0900

    카테고리 vo에 board_type 추가, common에 페이징 클래스 추가, board 영역 작업

--- hellomentor/src/main/java/com/kh/hellomentor/board/model/service/BoardService.java
+++ hellomentor/src/main/java/com/kh/hellomentor/board/model/service/BoardService.java
@@ -1,12 +1,84 @@
 package com.kh.hellomentor.board.model.service;
 
+import java.util.List;
+import java.util.Map;
+
 import com.kh.hellomentor.board.model.vo.Board;
 import com.kh.hellomentor.board.model.vo.Reply;
-
-import java.util.List;
 
 public interface BoardService {
     List<Board> getPostsByUserNo(int userNo);
 
     List<Reply> getReplyByUserNo(int userNo);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+	public List<Board> selectNoticeList(int currentPage, Map<String, Object> paramMap);
+	
+	public int selectListCount(Map<String, Object> paramMap);
 }
