1159a8a43b2a0750803c8c7e575b228bcde127bc
DESKTOP-6GHSRC0\offenderz <imjaelin@gmail.com>
2023-09-12 20:20:14 +0900

    집커밋

--- hellomentor/src/main/java/com/kh/hellomentor/board/controller/BoardController.java
+++ hellomentor/src/main/java/com/kh/hellomentor/board/controller/BoardController.java
@@ -1,55 +1,38 @@
 package com.kh.hellomentor.board.controller;
 
 import java.io.File;
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-=======
-import java.util.*;
->>>>>>> 42b6222ba4db5b70f7496f560ed53eacff73f8bf
 import java.util.stream.Collectors;
 
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.kh.hellomentor.matching.model.vo.StudyApplicant;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-=======
 import org.springframework.web.bind.annotation.*;
->>>>>>> 42b6222ba4db5b70f7496f560ed53eacff73f8bf
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.hellomentor.board.model.service.BoardService;
-import com.kh.hellomentor.board.model.vo.Answer;
 import com.kh.hellomentor.board.model.vo.Attachment;
 import com.kh.hellomentor.board.model.vo.Board;
-import com.kh.hellomentor.board.model.vo.Free;
 import com.kh.hellomentor.board.model.vo.Inquiry;
-import com.kh.hellomentor.board.model.vo.Knowledge;
 import com.kh.hellomentor.board.model.vo.Reply;
 import com.kh.hellomentor.common.Utils;
 import com.kh.hellomentor.member.controller.MemberController;
 import com.kh.hellomentor.member.model.vo.Member;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
-@Slf4j
 @SessionAttributes({"loginUser"})
 public class BoardController {
 
