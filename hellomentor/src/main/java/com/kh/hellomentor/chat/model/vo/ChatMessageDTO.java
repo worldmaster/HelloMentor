1dbbe4dc91c04274bba58ef933305de56c3a2c7b
shin10075 <shin10075@gmail.com>
2023-09-19 17:43:03 +0900

    chat profile 실험

--- hellomentor/src/main/java/com/kh/hellomentor/chat/model/vo/ChatMessageDTO.java
+++ hellomentor/src/main/java/com/kh/hellomentor/chat/model/vo/ChatMessageDTO.java
@@ -10,6 +10,7 @@ public class ChatMessageDTO {
     private String roomId;
     private String writer;
     private int userNo;
+    private String profile;
     private String message;
     private String sendDateTime;
 }
