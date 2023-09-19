62111f3464c584911e93e78220a1c689fbf2ee19
shin10075 <shin10075@gmail.com>
2023-09-19 14:08:07 +0900

    chatting socket 연결

---
+++ hellomentor/src/main/java/com/kh/hellomentor/chat/model/vo/ChatMessageDTO.java
@@ -0,0 +1,14 @@
+package com.kh.hellomentor.chat.model.vo;
+
+
+import lombok.*;
+
+
+@Getter
+@Setter
+public class ChatMessageDTO {
+    private String roomId;
+    private String writer;
+    private String message;
+    private String sendDateTime;
+}
