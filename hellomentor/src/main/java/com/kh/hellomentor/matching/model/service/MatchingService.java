af5ee567d516ba604bb5999822ab6365ce21b2cd
shin10075 <shin10075@gmail.com>
2023-09-20 16:22:40 +0900

    재린님 멘토링 매칭

--- hellomentor/src/main/java/com/kh/hellomentor/matching/model/service/MatchingService.java
+++ hellomentor/src/main/java/com/kh/hellomentor/matching/model/service/MatchingService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 
+import com.kh.hellomentor.matching.model.vo.Matching;
 import com.kh.hellomentor.matching.model.vo.Mentoring;
 import com.kh.hellomentor.member.model.vo.Follow;
 import com.kh.hellomentor.member.model.vo.Member;
@@ -40,4 +41,24 @@ public interface MatchingService {
     int follow(Follow follow);
 
     int unfollow(Follow follow);
+
+    List<Member> getMentorList(int userNo);
+
+    List<Profile> getMentorProfileList(int userNo);
+
+    List<Mentoring> getMentoringList(int userNo);
+
+    List<Matching> getMatchingList(int userNo);
+
+    List<Member> getMentorList2(int userNo);
+
+    List<Profile> getMentorProfileList2(int userNo);
+
+    List<Mentoring> getMentoringList2(int userNo);
+
+    List<Matching> getMatchingList2(int userNo);
+
+    void mentoring_cancel(int userNo, int regisNo);
+
+    void mentoring_accept(int userNo, int regisNo, int loginuserNo);
 }
